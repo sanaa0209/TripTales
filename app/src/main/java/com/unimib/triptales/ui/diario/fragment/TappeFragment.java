8f84390839df78b9fa3a405feff9ab986a179151
attiachristena <c.attia@campus.unimib.it>
2024-12-16 18:23:18 +0100

    Aggiunto scheletro schermata diario e dialog per aggiungere una nuova tappa

--- app/src/main/java/com/unimib/triptales/ui/diario/fragment/TappeFragment.java
+++ app/src/main/java/com/unimib/triptales/ui/diario/fragment/TappeFragment.java
@@ -1,35 +1,72 @@
 package com.unimib.triptales.ui.diario.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.unimib.triptales.AddTappaFragment;
 import com.unimib.triptales.R;
 
 public class TappeFragment extends Fragment {
-    public TappeFragment() {
-        // Required empty public constructor
-    }
 
-    public static TappeFragment newInstance(String param1, String param2) {
-        TappeFragment fragment = new TappeFragment();
-        Bundle args = new Bundle();
-        return fragment;
+    private View cardTappa;
+    private View addTappa;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tappe, container, false);
+
+        // Recupera il nome del diario passato con il Bundle
+        Bundle args = getArguments();
+        if (args != null) {
+            String diaryName = args.getString("diary_name");
+
+            // Trova la TextView e imposta il nome del diario
+            TextView diaryTitle = view.findViewById(R.id.diary_title);
+            if (diaryName != null) {
+                diaryTitle.setText(diaryName);
+            }
+        }
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tappe, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Card per aggiungere una nuova tappa
+        FloatingActionButton addTappa = view.findViewById(R.id.addTappaButton);
+        cardTappa = view.findViewById(R.id.cardAddTappa);
+
+        addTappa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    // Sostituisci il Fragment corrente con AddTappaFragment
+                    Fragment addTappaFragment = new AddTappaFragment();
+                    getParentFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, addTappaFragment)
+                            .addToBackStack(null)
+                            .commit();
+                } catch (Exception e) {
+                    Log.e("CreateTappaActivity", "Error opening AddTappaFragment", e);
+                    // Mostra un messaggio di errore all'utente o esegui altre azioni appropriate
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/unimib/triptales/ui/homepage/DialogAddDiaryActivity.java
+++ app/src/main/java/com/unimib/triptales/ui/homepage/DialogAddDiaryActivity.java
@@ -1,6 +1,5 @@
 package com.unimib.triptales.ui.homepage;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,20 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.unimib.triptales.R;
-import com.unimib.triptales.ui.homepage.fragment.CalendarFragment;
-import com.unimib.triptales.ui.homepage.fragment.MapFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class DialogAddDiaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_dialog_add_diary);
     }
 }
