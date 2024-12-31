3167e66b352d5148ac05bc3b17d432594a7c0583
Leslyz3024 <164195423+Leslyz3024@users.noreply.github.com>
2024-12-31 16:38:08 +0100

    updated intro with dotIndicator

---
+++ app/src/main/java/com/unimib/triptales/ui/intro/IntroFinishActivity.java
@@ -0,0 +1,38 @@
+package com.unimib.triptales.ui.intro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.unimib.triptales.R;
+import com.unimib.triptales.ui.login.LoginActivity;
+
+public class IntroFinishActivity extends AppCompatActivity {
+
+    Button startButton;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_finish);
+
+
+        startButton = findViewById(R.id.startButton);
+
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(IntroFinishActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
