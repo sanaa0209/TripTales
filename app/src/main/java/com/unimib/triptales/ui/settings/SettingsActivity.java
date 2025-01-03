ef77abfa15b97e3486da0beed335ef3eede4092e
Leslyz3024 <164195423+Leslyz3024@users.noreply.github.com>
2025-01-03 22:49:22 +0100

    fragment_settings.xml updated

--- app/src/main/java/com/unimib/triptales/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/unimib/triptales/ui/settings/SettingsActivity.java
@@ -17,10 +17,6 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
