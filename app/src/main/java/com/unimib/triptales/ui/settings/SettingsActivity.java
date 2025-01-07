c246dc0f04512ffd8c266015a92119fddd382934
Leslyz3024 <164195423+Leslyz3024@users.noreply.github.com>
2025-01-07 16:18:06 +0100

    i bottoni funzionano

--- app/src/main/java/com/unimib/triptales/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/unimib/triptales/ui/settings/SettingsActivity.java
@@ -2,18 +2,23 @@ package com.unimib.triptales.ui.settings;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.widget.Toolbar;
 
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.unimib.triptales.R;
+import com.unimib.triptales.adapters.SettingsAdapter;
+
 
 public class SettingsActivity extends AppCompatActivity {
 
-    ViewPager slideViewPager;
+    ViewPager2 viewPager2;
+    SettingsAdapter viewPagerAdapter;
+    //LinearLayout rootLayoutDiary;
+    Toolbar toolbar;
+    BottomNavigationView bottom_navigation;
 
 
     @Override
@@ -21,6 +26,13 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        toolbar = findViewById(R.id.toolbar);
+        bottom_navigation = findViewById(R.id.bottom_navigation);
+        viewPager2 = findViewById(R.id.settingsSlider);
 
+        viewPagerAdapter = new SettingsAdapter(this);
+        viewPager2.setAdapter(viewPagerAdapter);
     }
+
+
 }
