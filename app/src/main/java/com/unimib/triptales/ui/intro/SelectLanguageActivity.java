b61f1822059c3e63a753ba1f665d8bd3417a4a15
Leslyz3024 <164195423+Leslyz3024@users.noreply.github.com>
2025-01-31 16:42:36 +0100

    aggiornamento

---
+++ app/src/main/java/com/unimib/triptales/ui/intro/SelectLanguageActivity.java
@@ -0,0 +1,26 @@
+package com.unimib.triptales.ui.intro;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.unimib.triptales.R;
+
+public class SelectLanguageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_select_language);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
