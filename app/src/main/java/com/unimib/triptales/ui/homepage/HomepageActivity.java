b6eb63686c7ee25d6f7d527ebd76534b84d490b6
attiachristena <c.attia@campus.unimib.it>
2025-02-13 22:55:51 +0100

    Eliminato errore nell'HomepageActivity

--- app/src/main/java/com/unimib/triptales/ui/homepage/HomepageActivity.java
+++ app/src/main/java/com/unimib/triptales/ui/homepage/HomepageActivity.java
@@ -130,7 +130,6 @@ public class HomepageActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        homeViewModel.loadRemoteDiaries();
     }
 
     private void switchFragment(Fragment fragment, String tag){
